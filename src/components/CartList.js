d01e196b88c4aae8dfdf2cbb223a5b1e48aeb669
Brindha-sree <srikannan547@gmail.com>
2023-11-10 15:35:47 +0530

    ok

--- src/components/CartList.js
+++ src/components/CartList.js
@@ -15,7 +15,7 @@ function CartList({ cart }) {
                 CART?.map((cartItem, cartindex) => {
                     return (
                         <div>
-                            <img src={cartItem.url} width={40} />
+                            <img src={cartItem.url} width={40}  alt="" />
                             <span> {cartItem.name} </span>
                             <button
                                 onClick={() => {
